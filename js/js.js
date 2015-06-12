7bf20f19d06689436d6b15910d70f505a766acca
Saleh Qadan <sqadan@Salehs-MacBook-Pro.local>
2015-06-12 16:56:02 -0700

    res form complete. res overlay fixed

--- js/js.js
+++ js/js.js
@@ -57,3 +57,32 @@ pauseButton.addEventListener("click", function() {
         pauseButton.innerHTML = "Paused";
     }
 })
+
+
+$('#reservationModal').on('shown.bs.modal', function () {
+    $('#reservationModal').focus()
+})
+
+$("#reservationBtn").onclick, function(){
+    $(this).scrollTop
+}
+
+
+$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
